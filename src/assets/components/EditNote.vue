3e8f00a95c1dd1c884c1f3bfd4144a62d7126088
rsvincent <rvincent87@gmail.com>
2023-12-14 18:19:53 -0500

    WIP components but buttons dont work

--- src/assets/components/EditNote.vue
+++ src/assets/components/EditNote.vue
@@ -4,25 +4,12 @@
 
 <script setup>
 import { ref } from 'vue';
+
+
+
 const editNote = (id) => {
-    fetch(url + id, {
-      method: "PATCH",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        title: noteTitle.value,
-        body: noteBody.value,
-       
-      }),
-    })
-      .then((res) => res.json())
-      .then(
-        (data) => {
-          
-          items.value = data;
-          displayNote();
-        }
-  
-        
-      );
+
+    
+
   };
   </script>
